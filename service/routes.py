da6a271d31aa9cbf22ff14e4f0fae2137114f5a4
vallestroski <vallestroski@gmail.com>
2026-02-28 10:53:48 -0500

    Fix flake8 formatting

--- service/routes.py
+++ service/routes.py
@@ -61,9 +61,9 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
+
 @app.route("/accounts", methods=["GET"])
-
-
 def list_accounts():
     """
     List all Accounts
@@ -83,9 +83,9 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
-
 def get_accounts(account_id):
     """
     Reads an Account
@@ -103,8 +103,6 @@ def get_accounts(account_id):
 
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-
-
 def update_accounts(account_id):
     """
     Update an Account
@@ -124,9 +122,8 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
-
 def delete_accounts(account_id):
     """
     Delete an Account
